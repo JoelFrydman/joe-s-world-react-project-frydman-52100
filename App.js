9d13285887fcb4c14a9e18fdd80d144b34b66322
Joel Frydman <joelfrydman@hotmail.com>
2023-04-18 16:27:12 -0300

    prueba lista de compras

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import { StatusBar } from 'expo-status-bar';
+import { Button, Image, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+      <TextInput placeholder='elemento de la lista' style={styles.input}/>
+      <Button
+      title='Presiona aqui'
+      onPress={()=>console.log("presiona aqui")}/>
+      </View>
+      <View>
+      <Image
+        style={styles.tinyLogo}
+        source={require('./assets/imgs/logo.png')}
+      />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+  },
+  
+  inputContainer:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+
+  input:{
+    borderBottomColor:"black",
+    borderBottomWidth:2,
+    width:200,
+  },
+  tinyLogo: {
+    width: 150,
+    height: 100,
   },
 });
